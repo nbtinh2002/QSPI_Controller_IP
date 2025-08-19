3f032a08915e7606b61015cb5207071f51a0263d
baotinh <nbtinh2002@gmail.com>
2025-08-20 00:04:18 +0700

    ver0 tx fifo

--- RTL/qspi_tx_fifo.v
+++ RTL/qspi_tx_fifo.v
@@ -1,35 +1,39 @@
-module tx_fifo #(
-    parameter FIFO_DEPTH      = 16
+module qspi_tx_fifo #(
+    parameter FIFO_DEPTH = 16
 )(
     input  wire  clk,	
     input  wire  resetn,	
 
     // Write from CSR
-    input  wire        tx_wen,
-    input  wire [31:0] tx_data,		
-    output wire        tx_full, 
-    output wire [3:0]  tx_level,
+    input  wire        fifo_wen,
+    input  wire [31:0] data_in,		
+    output wire        fifo_full, 
+    output wire [3:0]  fifo_level,
 
     // Read to FSM
-    input  wire        tx_ren, 
-    output reg  [7:0]  tx_data,
-    output wire        tx_empty
+    input  wire        fifo_ren, 
+    output reg  [7:0]  data_out,
+    output wire        fifo_empty
 );
-    reg [7:0] fifo_mem[0:FIFO_DEPTH-1];// FIFO_DEPTH(=16) bytes
-    reg [5:0] wr_ptr; 
-    reg [5:0] rd_ptr; 
-    reg [5:0] level_cnt;
+    reg [7:0] fifo_mem[0:FIFO_DEPTH-1];
+    reg [4:0] wr_ptr, rd_ptr; 
+    reg [4:0] level_cnt;
 
-    assign fifo_empty = (level == 0);
-    assign fifo_full  = (level == FIFO_DEPTH);
-    assign level = level_cnt;
+    assign fifo_empty = (wr_ptr == rd_ptr); 
+    assign fifo_full = (fifo_level > FIFO_DEPTH - 4);
+    wire [4:0] level_diff = (wr_ptr[4] != rd_ptr[4]) ? (wr_ptr[3:0] + FIFO_DEPTH - rd_ptr[3:0]) : 
+                                                       (wr_ptr[3:0] - rd_ptr[3:0]);
+    assign fifo_level = level_diff[3:0];
 
     // Write
     always @(posedge clk or negedge resetn) begin
         if (!resetn) begin
             wr_ptr <= 5'd0;
-        end else if (wen && !fifo_full) begin
-            fifo_mem[wr_ptr[FIFO_ADDR_WIDTH-1:0]] <= data_in;
+        end else if (fifo_wen && !fifo_full) begin
+            fifo_mem[wr_ptr[3:0]]     <= data_in[32:24];
+            fifo_mem[wr_ptr[3:0] + 1] <= data_in[23:16];
+            fifo_mem[wr_ptr[3:0] + 2] <= data_in[15:8];
+            fifo_mem[wr_ptr[3:0] + 3] <= data_in[7:0];
             wr_ptr <= wr_ptr + 5'd4;                     
         end
     end
@@ -38,24 +42,10 @@ module tx_fifo #(
     always @(posedge clk or negedge resetn) begin
         if (!resetn) begin
             rd_ptr   <= 5'd0;
-            data_out <= 5'd0;
-        end else if (ren && !fifo_empty) begin
-            data_out <= fifo_mem[rd_ptr[FIFO_ADDR_WIDTH-1:0]];
+            data_out <= 8'd0;
+        end else if (fifo_ren && !fifo_empty) begin
+            data_out <= fifo_mem[rd_ptr[3:0]];
             rd_ptr   <= rd_ptr + 5'd1;
         end
     end
-
-    // Level counter
-    always @(posedge clk or negedge resetn) begin
-        if (!resetn) begin
-            level_cnt <= 5'd0;
-        end else if(wen && !fifo_full) begin
-            level_cnt <= level_cnt + 5'd4;
-        end else if(ren && !fifo_empty) begin
-            level_cnt <= level_cnt -5'd1;
-        end else begin
-            level_cnt <= level_cnt;
-        end 
-    end
-
 endmodule
