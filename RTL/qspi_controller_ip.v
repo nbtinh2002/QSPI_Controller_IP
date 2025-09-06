2a396deb1e8e6a16189a472b103aac369d87e331
baotinh <nbtinh2002@gmail.com>
2025-09-06 19:59:57 +0700

    nothing

--- RTL/qspi_controller_ip.v
+++ RTL/qspi_controller_ip.v
@@ -33,49 +33,34 @@ module qspi_controller_ip
 	output wire                      wp_n,
 
 	// AXI4 Master Interface
-//	output wire [3:0]                m_awid,
     output wire [AXI_ADDR_WIDTH-1:0] m_awaddr,
-//    output wire [7:0]                m_awlen,
-//    output wire [2:0]                m_awsize, 
-//    output wire [1:0]                m_awburst,
     output wire                      m_awvalid,
     input  wire                      m_awready,
     // Write Data Channel
     output wire [DATA_WIDTH-1:0]     m_wdata,
     output wire [DATA_WIDTH/8-1:0]   m_wstrb,
-//    output wire                      m_wlast,
-//    output wire                      m_wuser,
     output wire                      m_wvalid,
     input  wire                      m_wready,
     // Write Response Channel
-//    input  wire [3:0]                m_bid,
-//    input  wire [1:0]                m_bresp,
-//    input  wire                      m_buser,
     input  wire                      m_bvalid,
     output wire                      m_bready,
     // Read Address Channel
-//    output wire [3:0]                m_arid,
     output wire [AXI_ADDR_WIDTH-1:0] m_araddr,
-//    output wire [7:0]                m_arlen,
-//    output wire [2:0]                m_arsize,
-//    output wire [1:0]                m_arburst,
     output wire                      m_arvalid,
     input  wire                      m_arready,
     // Read Data Channel
-//    input  wire [3:0]                m_rid,
     input  wire [DATA_WIDTH-1:0]     m_rdata,
-//    input  wire [1:0]                m_rresp,
-//    input  wire                      m_rlast,
-//    input  wire                      m_ruser,
     input  wire                      m_rvalid,
     output wire                      m_rready
+	//[1]
 );
 	// Internal signals
+
 	wire		hold_en, wp_en;
 	wire 		enable, xip_en, quad_en, cpol, cpha, lsb_first, cmd_trigger;
 	wire 		ce_busy,  ce_done;
-	wire 		timeout, overrun, underrun;
-	wire 		err_set = timeout | overrun | underrun;
+	wire 		timeout, overrun, underrun, axi_err;
+	wire 		err_set = timeout | overrun | underrun | axi_err ;
 	wire [2:0]	clk_div;
 	wire 		cs_auto, cs_level;
 	wire [1:0]	cs_delay;
@@ -97,16 +82,14 @@ module qspi_controller_ip
     wire [31:0] dma_fifo_rdata, dma_fifo_wdata;
 	wire 		incr_addr;
 
-    //signal for mux control
     wire 		fifo_rx_sel_ren, fifo_rx_sel_empty, fifo_tx_sel_wen, fifo_tx_sel_full;
-    wire[31:0] fifo_rx_sel_rdata, fifo_tx_sel_wdata;
+    wire[31:0] 	fifo_rx_sel_rdata, fifo_tx_sel_wdata;
 
-	//csr source addr and len, dam_dir is csr dma_cfg[4]
-	wire 		dma_dir; // 0 read from fifo, 1 write to fifo
-    wire [31:0] dma_addr; // souce addr dma
-    wire [31:0] dma_len; //len for dma
 
-	//dma with ce: dma done and start signal
+	wire 		dma_dir;
+    wire [31:0] dma_addr;
+    wire [31:0] dma_len;
+
 	wire [3:0] burst_size;
 	wire dma_ce_done, dma_start;
 	wire [6:0] byte_level;
@@ -121,22 +104,14 @@ module qspi_controller_ip
 		.hold_en_o(hold_en), .wp_en_o(wp_en),
 		// CTRL							// STATUS					// INT_STAT							// CLK_DIV, CS_CTRL
 		.enable_o(enable),				.busy_i(ce_busy),			.cmd_done_set_i(ce_done),			.clk_div_o(clk_div), 
-		.xip_en_o(xip_en), 				.xip_active_i(),			.dma_done_set_i(dma_ce_done),		.cs_auto_o(cs_auto), 
+		.xip_en_o(xip_en), 				.xip_active_i(1'b0),		.dma_done_set_i(dma_ce_done),		.cs_auto_o(cs_auto), 
 		.quad_en_o(quad_en), 			.cmd_done_i(ce_done),		.err_set_i(err_set),				.cs_level_o(cs_level), 
 		.cpol_o(cpol), 					.dma_done_i(dma_ce_done),	.fifo_tx_empty_set_i(tx_empty),		.cs_delay_o(cs_delay),
 		.cpha_o(cpha), 												.fifo_rx_full_set_i(rx_full), 
 		.lsb_first_o(lsb_first), 
 		.cmd_trigger_o(cmd_trigger), 
-		.dma_en_o(dma_en),
-		// XIP_CFG						//XIP_CMD
-		.xip_cmd_lanes_o(), 			.xip_read_op_o(),
-		.xip_addr_lanes_o(), 			.xip_write_op_o(),
-		.xip_data_lanes_o(), 			.xip_mode_bits_o(),
-		.xip_addr_bytes_o(),
-		.xip_mode_en_o(),
-		.xip_dummy_cycles_o(), 
-		.xip_cont_read_o(), 
-		.xip_write_o(), 
+		.dma_en_o(dma_en),		
+		//[2]
 		//CMD_CFG							//CMD_OP						//CMD_ADDR, CMD_LEN, CMD_EXTRA_DUMMY
 		.cmd_cmd_lanes_o(cmd_lanes), 		.cmd_opcode_o(cmd_opcode),		.cmd_addr_o(cmd_addr),
 		.cmd_addr_lanes_o(addr_lanes),		.cmd_mode_bits_o(mode_bits), 	.cmd_len_o(cmd_len),
@@ -149,19 +124,18 @@ module qspi_controller_ip
 		.dma_burst_size_o(burst_size), 	.dma_addr_o(dma_addr),
 		.dma_dir_o(dma_dir), 			.dma_len_o(dma_len),
 		.dma_incr_addr_o(incr_addr),
-		// FIFO_STAT
-		.tx_level_i(tx_level), 		.rx_level_i(rx_level),  
-		.tx_empty_i(tx_empty),		.rx_full_i(rx_full),
 		// ERR_STAT
 		.timeout_i(timeout), 
 		.overrun_i(overrun), 
 		.underrun_i(underrun), 
-		.axi_err_i(),
-
-		// FIFO signals
-		.tx_full_i(tx_full), 		.rx_empty_i(rx_empty),
-		.tx_data_o(tx_data), 		.rx_data_i(rx_data),
-		.tx_wen_o(tx_wen), 			.rx_ren_o(rx_ren)	
+		.axi_err_i(axi_err),
+		// FIFO_STAT					// FIFO signal
+		.tx_level_i(tx_level),			.tx_full_i(tx_full), 
+		.tx_empty_i(tx_empty),			.tx_data_o(tx_data), 
+ 		.rx_level_i(rx_level),			.tx_wen_o(tx_wen),  
+		.rx_full_i(rx_full),			.rx_empty_i(rx_empty),
+										.rx_data_i(rx_data),
+										.rx_ren_o(rx_ren)	
 	);
 	
 	// CE
@@ -189,7 +163,6 @@ module qspi_controller_ip
 		.empty(tx_empty), 
 		.level(tx_level)
 	);
-
 	qspi_fifo #(.FIFO_DEPTH(FIFO_DEPTH),.WR_BYTES(1),.RD_BYTES(4)) rx_fifo_inst (
 		.clk(clk), .resetn(resetn), 
 		.data_in(rx_data_fifo), 		
@@ -215,14 +188,21 @@ module qspi_controller_ip
 		.cs_auto(1'b1), .cs_level(cs_level), .cs_delay(cs_delay),
 		// Start/Done
 		.start(ce_start), .done(qspi_done),
-		// CMD & XIP field				// XIP field					// CMD field
-		.cmd_lanes(cmd_lanes), 			.xip_cont_read(),				.cmd_dir(cmd_dir),
-		.addr_lanes(addr_lanes), 		.xip_write_en(),				.cmd_opcode(cmd_opcode),
-		.data_lanes(data_lanes), 		.xip_read_op(),					.cmd_addr(cmd_addr),
-		.addr_bytes(addr_bytes),		.xip_write_op(),				.cmd_len(cmd_len),
-		.mode_en(mode_en), 												.cmd_extra_dummy(cmd_extra_dummy),
+		// CMD & XIP field
+		.cmd_lanes(cmd_lanes),
+		.addr_lanes(addr_lanes),
+		.data_lanes(data_lanes),
+		.addr_bytes(addr_bytes),
+		.mode_en(mode_en),
 		.dummy_cycles(dummy_cycles), 
 		.mode_bits(mode_bits),
+		//[3]
+		// CMD field
+		.cmd_dir(cmd_dir),
+		.cmd_opcode(cmd_opcode),
+		.cmd_addr(cmd_addr),
+		.cmd_len(cmd_len),
+		.cmd_extra_dummy(cmd_extra_dummy),
 		// TX FIFO signals (IP -> flash),// RX FIFO signals (flash -> IP)
 		.tx_empty(tx_empty), 				.rx_full(rx_full),
 		.tx_data_fifo(tx_data_fifo), 		.rx_data_fifo(rx_data_fifo),
@@ -282,15 +262,38 @@ module qspi_controller_ip
         .axi_arvalid(m_arvalid), .axi_araddr(m_araddr), .axi_arready(m_arready),
         .axi_rvalid(m_rvalid), .axi_rdata(m_rdata), .axi_rready(m_rready)
     );
-
-	assign m_awid    = 4'd0;
-	assign m_awlen   = 8'd0;
-	assign m_awsize  = 3'd2;   // 4-byte beat
-	assign m_awburst = 2'b01;  // INCR
-	assign m_wlast   = 1'b1;
-	assign m_wuser   = 1'b0;
-	assign m_arid    = 4'd0;
-	assign m_arlen   = 8'd0;
-	assign m_arsize  = 3'd2;
-	assign m_arburst = 2'b01;
 endmodule
+//[1]
+//	  output wire [3:0]                m_awid,
+//    output wire [7:0]                m_awlen,
+//    output wire [2:0]                m_awsize, 
+//    output wire [1:0]                m_awburst,
+//    output wire                      m_wlast,
+//    output wire                      m_wuser,
+//    input  wire [3:0]                m_bid,
+//    input  wire [1:0]                m_bresp,
+//    input  wire                      m_buser,
+//    output wire [3:0]                m_arid,
+//    output wire [7:0]                m_arlen,
+//    output wire [2:0]                m_arsize,
+//    output wire [1:0]                m_arburst,
+//    input  wire [3:0]                m_rid,
+//    input  wire [1:0]                m_rresp,
+//    input  wire                      m_rlast,
+//    input  wire                      m_ruser,
+//[2]
+//		// XIP_CFG						//XIP_CMD
+//		.xip_cmd_lanes_o(), 			.xip_read_op_o(),
+//		.xip_addr_lanes_o(), 			.xip_write_op_o(),
+//		.xip_data_lanes_o(), 			.xip_mode_bits_o(),
+//		.xip_addr_bytes_o(),
+//		.xip_mode_en_o(),
+//		.xip_dummy_cycles_o(), 
+//		.xip_cont_read_o(), 
+//		.xip_write_o(), 
+//[3]
+//		// XIP field
+// 		.xip_cont_read(),
+//		.xip_write_en(),
+//		.xip_read_op(),
+//		.xip_write_op(),
